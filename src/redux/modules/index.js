e03ce6ec1bbe018f4fe1ed384aecd4c9cfa50740
Stefan Schult <stefan@schult.mobi>
2016-03-28 17:50:14 +0200

    readd missing files

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
-import players from './players';
+import players from '../../players';
 import {_reducers as games} from '../../games/games';
 
 export default combineReducers({
